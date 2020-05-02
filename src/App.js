582c56e109ec571b891d4e5d73de8fff8675e52f
Abhijeet Sinha <abhijeetaman007@gmail.com>
2020-05-02 08:37:00 +0530

    New files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './style.css'
+import image from './spotify.png'
+import songs from'./songlist'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="" src={image}/>
+      <button className="button">P A U S E</button>
+     <h1 className="text">Songs</h1>
+     <div>
+  {songs.map(item => <div>
+    <h2>{item.id}. {item.name}
+    <br></br>
+  <h5>Artist: {item.artist}</h5>
+    <hr></hr>
+    </h2>
+    </div>)}
+     </div>
     </div>
   );
 }
